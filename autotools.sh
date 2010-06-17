7418bee3ec047df7d4ca870e95faef936780982d
Timon <timon@timon-laptop.(none)>
2010-06-17 21:31:04 +0200

    Fixed small glitch in output

--- autotools.sh
+++ autotools.sh
@@ -3,8 +3,9 @@
 # This script calls the autotools to generate the build infrastructure
 # After this was done, the developer can use './configure' and 'make'
 
-aclocal    && \
-autoconf   && \
-autoheader && \
-automake   && \
-echo -e "Autotools completed successfully.\nUse \"./configure\" and \"make\" to build Chronos!"
+aclocal                                  && \
+autoconf                                 && \
+autoheader                               && \
+automake                                 && \
+echo "Autotools completed successfully." && \
+echo "Use \"./configure\" and \"make\" to build Chronos!"
